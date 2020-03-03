181f850ee21d8de1fdf9c133e99a118915e8c934
CedricDeMits <cedric.demits@efeso.com>
2020-03-03 11:54:58 +0100

    made issuerequest a class whatever fuck it

--- lib/gitlabarty.rb
+++ lib/gitlabarty.rb
@@ -4,7 +4,23 @@ require 'gitlabarty/configuration'
 
 module Gitlabarty
   def self.create_issue(options = {})
-    Gitlabarty::IssueRequest.create(options)
+    request = Gitlabarty::IssueRequest.new(params: options)
+    request.create
+  end
+
+  def self.read_issue(id, *options)
+    request = Gitlabarty::IssueRequest.new(id: id, params: options)
+    request.read
+  end
+
+  def self.update_issue(id, *options)
+    request = Gitlabarty::IssueRequest.new(id: id, params: options)
+    request.update
+  end
+
+  def self.delete_issue(id, *options)
+    request = Gitlabarty::IssueRequest.new(id: id, params: options)
+    request.delete
   end
 
   def self.configuration
--- lib/gitlabarty/issuerequest.rb
+++ lib/gitlabarty/issuerequest.rb
@@ -1,51 +1,58 @@
 require 'net/http'
 
 module Gitlabarty
-  module IssueRequest
-    def self.read(params = {})
-      return unless params[:id]
+  class IssueRequest
+    attr_accessor :id,
+                  :params
 
-      url = "#{Gitlabarty.configuration.url}/#{Gitlabarty.configuration.id}/issues/#{params[:id]}"
-
-      get = Net::HTTP::Get.new("#{url}?#{URI.encode_www_form(params)}")
-      get['PRIVATE-TOKEN'] = Gitlabarty.configuration.private_token
-
-      send_request(get)
+    def issue_url
+      "#{Gitlabarty.configuration.url}/#{Gitlabarty.configuration.id}/issues"
     end
 
-    def self.create(params = {})
+    def create
       default_params = { title: 'New Issue' }
       params = default_params.merge(post_params(params))
-      url = "#{Gitlabarty.configuration.url}/#{Gitlabarty.configuration.id}/issues"
 
-      post = Net::HTTP::Post.new("#{url}?#{URI.encode_www_form(params)}")
+      post = Net::HTTP::Post.new("#{issue_url}?#{URI.encode_www_form(params)}")
       post['PRIVATE-TOKEN'] = Gitlabarty.configuration.private_token
 
-      send_request(post)
+      response = send_request(post)
+      puts "#{response.body}"
     end
 
-    def self.update(params = {})
-      return unless params[:id]
+    def read
+      return unless id
+
+      get = Net::HTTP::Get.new("#{issue_url}/#{id}?#{URI.encode_www_form(params)}")
+      get['PRIVATE-TOKEN'] = Gitlabarty.configuration.private_token
+
+      response = send_request(get)
+      puts "#{response.body}"
+    end
+
+    def update
+      return unless id
 
       params = put_params(params)
-      url = "#{Gitlabarty.configuration.url}/#{Gitlabarty.configuration.id}/issues/#{params[:id]}"
 
-      put = Net::HTTP::Put.new("#{url}?#{URI.encode_www_form(params)}")
+      put = Net::HTTP::Put.new("#{issue_url}/#{id}?#{URI.encode_www_form(params)}")
       put['PRIVATE-TOKEN'] = Gitlabarty.configuration.private_token
 
-      send_request(put)
+      response = send_request(put)
+      puts "#{response.body}"
     end
 
-    def self.delete(params = {})
-      return unless params[:id]
+    def delete
+      return unless id
 
-      url = "#{Gitlabarty.configuration.url}/#{Gitlabarty.configuration.id}/issues/#{params[:id]}"
+      del = Net::HTTP::Delete.new("#{issue_url}/#{id}")
+      del['PRIVATE-TOKEN'] = Gitlabarty.configuration.private_token
 
-      delete = Net::HTTP::Delete.new("#{url}?#{URI.encode_www_form(params)}")
-      delete['PRIVATE-TOKEN'] = Gitlabarty.configuration.private_token
-
-      send_request(delete)
+      response = send_request(del)
+      puts "#{response.body}"
     end
+
+    private
 
     def post_params(params)
       params.slice(:title, :description, :confidential, :assignee_ids,
@@ -60,10 +67,12 @@ module Gitlabarty
     end
 
     def send_request(request)
+      response = nil
       uri = URI(Gitlabarty.configuration.url)
       Net::HTTP.start(uri.host, uri.port, use_ssl: true) do |http|
-        http.request request
+        response = http.request request
       end
+      response
     end
   end
 end
